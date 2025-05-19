7f17706ccbd9d558c1479a94d383fe377ea85d39
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-05-19 18:07:21 -0300

    Update favoritos.js

--- src/servicos/favoritos.js
+++ src/servicos/favoritos.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const favoritosAPI= axios.create({baseURL: "http://localhost:8000/favoritos"});
+const favoritosAPI= axios.create({baseURL: import.meta.env.VITE_API_BASE_URL + '/favoritos'});
 
 async function getFavoritos(){
     const response = await favoritosAPI.get('/');
